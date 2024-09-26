6d42b03a0422874c4c3b1a8a51b2d307b1e181da
aliifam <aliif.arief.m@gmail.com>
2024-09-26 16:30:34 +0700

    feat: add react helmet async for seo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,24 @@
+import { Helmet } from "react-helmet-async";
+import { useNavigate } from "react-router-dom";
+
 export const Home = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
-      <h1>Home</h1>
+      <Helmet>
+        <title>Al-Matsurat Online</title>
+        <meta
+          name="description"
+          content="Al-Matsurat Online adalah aplikasi untuk membantu membaca Al-Matsurat setiap pagi dan petang"
+        />
+        <meta
+          name="keywords"
+          content="Al-Matsurat, Al-Matsurat Online, Al-Matsurat website, Al-Matsurat app, Al-Matsurat sugro, Al-Matsurat pagi, Al-Matsurat petang, Al-Matsurat kubro"
+        />
+      </Helmet>
+      <h1 className="font-bold">Al-Matsurat Online</h1>
+      <p>biasakan membaca almatsurat setiap pagi dan petang</p>
     </div>
   );
 };
