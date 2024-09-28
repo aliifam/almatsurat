c0ec2a5d5221aab04be7f0d462cfd758f8bded91
aliifam <aliif.arief.m@gmail.com>
2024-09-29 00:39:37 +0700

    feat: pwa refresh new content

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,16 @@ import App from "./App.tsx";
 import "./index.css";
 import { HelmetProvider } from "react-helmet-async";
 import { ThemeContextProvider } from "./context/ThemeContext.tsx";
+import { registerSW } from "virtual:pwa-register";
+
+// add this to prompt for a refresh
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New content available. Reload?")) {
+      updateSW(true);
+    }
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
