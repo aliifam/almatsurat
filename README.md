b2cf6d01bb6753d0e2f064beb8fa2f5e0d0c1513
aliifam <aliif.arief.m@gmail.com>
2024-09-28 23:03:02 +0700

    feat: convert to PWA

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This is a web app version of Al Matsurat, a compilation of morning and evening dzikr written by Imam Hasan Al Banna. This app is built using ReactJS and TailwindCSS.
 
 This project open for contribution, feel free to fork and submit a pull request.
+
+[☕ Traktir Kopi](https://trakteer.id/aliifam/tip)
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,62 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 // https://vitejs.dev/config/
 declare const __dirname: string; // Add this line to declare __dirname
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      // add this to cache all the imports
+      workbox: {
+        globPatterns: ["**/*"],
+      },
+      // add this to cache all the
+      // static assets in the public folder
+      includeAssets: ["**/*"],
+      manifest: {
+        theme_color: "#000000",
+        background_color: "#000000",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        short_name: "Al-Matsurat",
+        description: "Al-Matsurat App",
+        name: "Al-Matsurat",
+        icons: [
+          {
+            src: "/icon/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/icon/android-chrome-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/icon/android-chrome-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: [
       {
